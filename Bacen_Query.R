03c81a14bb0c80e243f394eb4b57c5147a3566cc
João Leonardo Rodrigues Santos <jotaleo.ce3@gmail.com>
2026-07-08 21:01:17 -0300

    Refactor bacen_query function and improve error handling

--- Bacen_Query.R
+++ Bacen_Query.R
@@ -5,29 +5,23 @@
 # --- Script by Paulo Icaro ---#
 
 # Carrega funções auxiliares localmente
-library(dplyr)
 source("Bacen_API.R")
 source("Bacen_URL.R")
+
+library(dplyr)
 
 bacen_query = function(bacen_series_code, bacen_series_name, start_date, end_date){
   
   for(i in seq_along(bacen_series_code)){
-  
     message(paste0('Extraindo ', '"', bacen_series_name[i], '"'))
     
     tryCatch(expr = {
-      
-      # --- Extraction --- #
       bacen_dataset_raw = bacen_api(url = bacen_url(bacen_series_code[i], start_date, end_date))
-  
-      # --- Grouping Columns --- #
       if(i == 1){
         bacen_dataset = bacen_dataset_raw
       } else {
         bacen_dataset = left_join(x = bacen_dataset, y = bacen_dataset_raw, by = join_by('data' == 'data'))
       }
-    
-      # --- Naming Headers --- #
       if(i == length(bacen_series_code)){
         colnames(bacen_dataset) = c('data', bacen_series_name)
       }
@@ -36,6 +30,5 @@ bacen_query = function(bacen_series_code, bacen_series_name, start_date, end_dat
       stop('Erro na extração: ', e$message, call. = FALSE)
     })
   }
-  
   return(bacen_dataset)
 }
